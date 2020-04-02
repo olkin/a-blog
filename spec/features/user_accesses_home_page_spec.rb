d4d98b7082fd403cdd21ff115ca161b2ccf53d02
olkin <pilip.olkin@gmail.com>
2020-04-01 21:20:37 -0600

    fix up some rspec issues, add helper

--- spec/features/user_accesses_home_page_spec.rb
+++ spec/features/user_accesses_home_page_spec.rb
@@ -8,18 +8,10 @@ feature 'Accessing home page ' do
   end
 
   scenario 'is successful for signed-in users' do
-    email = 'tom.hanks@example.com'
-    password = 'password'
-
     # TODO: use factory_bot
-    User.create(email: email, password: password)
+    user = User.create(email: 'tom.hanks@example.com', password: 'password')
 
-    # TODO: move to a helper
-    visit sign_in_path
-    fill_in 'Email', with: email
-    fill_in 'Password', with: password
-    click_button 'Sign in'
-
+    sign_in_as(user)
     visit root_path
     expect(page).to have_content 'Wohoo'
     expect(page).to have_content 'Signed in as: tom.hanks@example.com'

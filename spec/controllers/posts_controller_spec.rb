47cefdae287155731edf2a2046aaa06c50413e87
Olga Pilipenco <olkin@users.noreply.github.com>
2020-04-16 16:10:51 -0600

    Edit delete posts (#8)
    * Add edit/remove functionality
    
    * Delete post from edit screen
    
    * add specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe PostsController do
   let(:user) { create :user }
+  let(:user_post) { create :post, user: user }
 
   before do
     sign_in_as(user)
   end
 
   describe 'POST #create' do
-    it 'assigns posts' do
+    it 'creates the post' do
       expect { post :create, params: { post: { body: 'hello', title: 'title' } } }.
           to change { user.posts.count }.by(1)
 
@@ -25,6 +26,33 @@ describe PostsController do
         expect(flash[:error]).to eq 'Oopsie'
         expect(response).to have_http_status :ok
       end
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'finds posts' do
+      get :edit, params: { id: user_post.to_param }
+
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe 'PATCH #update' do
+    it 'finds posts' do
+      put :update, params: { id: user_post.to_param, post: { body: 'hello', title: 'title' } }
+
+      expect(response).to have_http_status :found
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:user_post) { create :post, user: user }
+
+    it 'finds posts' do
+      expect { delete :destroy, params: { id: user_post.to_param } }.
+          to change { user.posts.count }.by(-1)
+
+      expect(response).to have_http_status :found
     end
   end
 end

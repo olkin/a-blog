b423f04c05bc9625e3a6831d65d4c0e56a0ae82d
olkin <pilip.olkin@gmail.com>
2020-05-11 14:30:38 -0600

    importing css to temporarily make webpacker happy

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,6 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import '_post.scss' // TODO: temp to please webpack?
 
 const Hello = props => (
   <div>Hello {props.name}!</div>

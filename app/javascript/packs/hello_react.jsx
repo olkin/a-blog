45d58814619f6fb4ab59394a45f400aba7dd25ec
olkin <pilip.olkin@gmail.com>
2020-05-11 14:44:43 -0600

    updated versions

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,7 +5,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import '_post.scss' // TODO: temp to please webpack?
 
 const Hello = props => (
   <div>Hello {props.name}!</div>

7a40a90b3c08f338b5cb7dd1788fb18cce600bd1
olkin <pilip.olkin@gmail.com>
2020-05-12 11:44:52 -0600

    add components to main area

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -5,6 +5,6 @@ import App from "../components/App";
 document.addEventListener("DOMContentLoaded", () => {
     render(
         <App />,
-        document.body.appendChild(document.createElement("div"))
+        document.getElementById('main_area').appendChild(document.createElement("div"))
     );
 });

7d174f4afe693cd0d6174d8f384cf06ee2415bbb
olkin <pilip.olkin@gmail.com>
2020-05-26 13:25:41 -0600

    add login/logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     #@posts = Post.system.order(created_at: :desc)
-    render json: 'all good'
+    render json: { message: 'all good' }, status: :ok
   end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < ApplicationController
+  def create
+    user = User.create! email: params[:user][:email],
+                        password: params[:user][:password],
+                        password_confirmation: params[:user][:password]
+
+    if user
+      session[:user_id] = user.id
+      render json: { user: user }, status: :created
+    else
+      render json: {}, status: :unprocessable_entity
+    end
+  end
+end

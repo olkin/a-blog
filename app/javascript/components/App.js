baf9967789987d3e0d3391717bbaef5d259ffea8
olkin <pilip.olkin@gmail.com>
2020-05-26 16:04:36 -0600

    add login request

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from "./Home";
 import Dashboard from "./Dashboard";
+import axios from 'axios';
 
 const App = () => {
     const [loggedInStatus, setLoggedInStatus] = useState('NOT_LOGGED_IN');
     const [user, setUser] = useState({});
+
+    const checkLoginStatus = () => {
+        axios.get('http://localhost:3000/logged_in',
+            {withCredentials: true}
+        ).then(response => {
+            if (response.data.logged_in && loggedInStatus === 'NOT_LOGGED_IN') {
+                setLoggedInStatus('LOGGED_IN');
+                setUser(response.data.user);
+            } else if (!response.data.logged_in && loggedInStatus === 'LOGGED_IN') {
+                setLoggedInStatus('NOT_LOGGED_IN');
+                setUser({});
+            }
+        }).catch(error => {
+            console.log("login error", error)
+        })
+    }
+
+    useEffect(checkLoginStatus, []);
 
     const handleLogin = (data) => {
         setLoggedInStatus('LOGGED_IN');
--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Registration from "./auth/Registration";
 import Login from "./auth/Login";
 

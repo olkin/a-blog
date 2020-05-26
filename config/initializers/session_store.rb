74c1b79d25db183d8e27d65d148f7818e8883663
olkin <pilip.olkin@gmail.com>
2020-05-26 13:28:49 -0600

    set cookies for prod/dev

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,8 @@
-Rails.application.config.session_store :cookie_store, key: '_a_blog_app', domain: 'vb-blog.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_a_blog_app',
+                                         domain: 'vb-blog.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_a_blog_app'
+end

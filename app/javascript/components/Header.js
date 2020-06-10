a16c791abf9ef5891d17e8b5a4e5f8913780dddb
olkin <pilip.olkin@gmail.com>
2020-06-10 16:24:27 -0600

    add header

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
 import axios from "axios";
 import userContext from "./userContext";
+import '../styles/Header.scss'
 
 function Header(props) {
     const userInfo = useContext(userContext);
@@ -17,17 +18,23 @@ function Header(props) {
     }
 
     return (
-        <div>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                {userInfo.user.email
-                    ? <li><Link to="/" onClick={handleLogout}>Sign out</Link></li>
-                    : <>
-                        <li><Link to='/sign_in'>Sign in</Link></li>
-                        <li><Link to='/sign_up'>Sign up</Link></li>
-                    </>
-                }
-            </ul>
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text"><Link to='/'>Home</Link></li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    {userInfo.user.email
+                        ? <li><Link to="/" onClick={handleLogout}>Sign out</Link></li>
+                        : <>
+                            <li><Link to='/sign_in'>Sign in</Link></li>
+                            <li><Link to='/sign_up'>Sign up</Link></li>
+                        </>
+                    }
+                </ul>
+            </div>
         </div>
     )
 }

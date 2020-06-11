b17438713a3048e11900977879e11ae3fe645a7d
olkin <pilip.olkin@gmail.com>
2020-06-11 13:22:09 -0600

    fonts in footer

--- app/javascript/components/Hero.js
+++ app/javascript/components/Hero.js
@@ -8,7 +8,10 @@ function Footer() {
             <div className="wrap">
                 <h1>Welcome to Home</h1>
                 <p>
-                    Cat ipsum dolor sit amet, lie in the sink all day but i vomit in the bed in the middle of the night. I want to go outside let me go outside nevermind inside is better scoot butt on the rug meowwww ask for petting, and furrier and even more furrier hairball groom forever, stretch tongue and leave it slightly out, blep, sun bathe. Reaches under door into adjacent room attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently side-eyes your "jerk" other hand while being petted , for in the middle of the night i crawl onto your chest and purr gently to help you sleep purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table the cat was chasing the mouse. That box? i can fit in that box. Annoy kitten brother with poking love you, then bite you hiiiiiiiiii feed me now. Pretend not to be evil. Run at 3am going to catch the red dot today going to catch the red dot today and make it to the carpet before i vomit mmmmmm howl on top of tall thing oooo! dangly balls! jump swat swing flies so sweetly to the floor crash move on wash belly nap stare out cat door then go back inside.
+                    Cat ipsum dolor sit amet,
+                    lie in the sink all day but i vomit in the bed in the middle of the night. I want to go outside let me go outside nevermind inside is better scoot butt on the rug meowwww ask for petting,
+                    and furrier and even more furrier hairball groom forever, stretch tongue and leave it slightly out, blep, sun bathe. Reaches under door into adjacent room attempt to leap between furniture but woefully
+                    miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently side-eyes your "jerk" other hand while being petted , for in the middle of the night i crawl onto your chest and purr gently to help you sleep purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table the cat was chasing the mouse.
                 </p>
             </div>
         </section>

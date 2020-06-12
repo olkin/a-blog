8cfe953efd84b6b4bfb9ef923b9422456fd02374
olkin <pilip.olkin@gmail.com>
2020-06-11 20:05:12 -0600

    try to relocate styles

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
 import axios from "axios";
 import userContext from "./userContext";
-import '../styles/Header.scss';
+import './Header.scss';
 import '../styles/GlobalStyles.scss';
 
 function Header(props) {

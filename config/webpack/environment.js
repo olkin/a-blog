49419e45b63d9f5d71e5b05556c91ad32271952f
olkin <pilip.olkin@gmail.com>
2020-06-12 11:25:45 -0600

    cleanup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,22 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-//
-//
-// module.exports = {
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.s[ac]ss$/i,
-//                 use: [
-//                     // Creates `style` nodes from JS strings
-//                     'style-loader',
-//                     // Translates CSS into CommonJS
-//                     'css-loader',
-//                     // Compiles Sass to CSS
-//                     'sass-loader',
-//                 ],
-//             },
-//         ],
-//     },
-// };

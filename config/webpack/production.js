ef56d451810ca6231e882c98b16448068d2ec120
olkin <pilip.olkin@gmail.com>
2020-06-12 11:22:34 -0600

    try webpacker config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const sassLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+    ],
+}
+
+environment.loaders.append('sass', sassLoader)
+
 module.exports = environment.toWebpackConfig()

5f4f636faabdf033888fd000f02555ee1d7c128a
Olga Pilipenco <pilip.olkin@gmail.com>
2020-06-27 17:11:09 -0600

    add data columns

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -6,8 +6,7 @@ function Home() {
     const userInfo = useContext(userContext);
 
     return (
-        <div className="grid-x">
-            <div className="cell large-8">
+    <>
                 {/*{userInfo.user.email*/}
                 {/*    ?*/}
                 {/*    <Link to="/posts/new" className="button">*/}
@@ -17,10 +16,7 @@ function Home() {
                 {/*}*/}
 
                 <Events/>
-            </div>
-            <div className="cell large-4">
-            </div>
-        </div>
+</>
     );
 }
 
--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -12,25 +12,31 @@ function Main(props) {
     return (
         <section className="main">
             <div className="wrap">
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact
-                           path='/sign_in'
-                           render={loginProps => (
-                               <Login {...loginProps}
-                                      handleSuccessfulAuth={props.handleLogin}
-                               />
-                           )}
-                    />
-                    <Route exact path='/sign_up' render={registrationProps => (
-                        <Registration {...registrationProps}
-                                      handleSuccessfulAuth={props.handleLogin}
-                        />
-                    )}/>
-                    <Route path="/posts" exact component={Home} />
-                    <Route path="/posts/new" exact component={NewPost} />
-                    <Route path="/posts/:id/edit" exact component={EditPost} />
-                </Switch>
+                <div className='grid-x'>
+                    <div className="cell large-8">
+                        <Switch>
+                            <Route exact path='/' component={Home}/>
+                            <Route exact
+                                   path='/sign_in'
+                                   render={loginProps => (
+                                       <Login {...loginProps}
+                                              handleSuccessfulAuth={props.handleLogin}
+                                       />
+                                   )}
+                            />
+                            <Route exact path='/sign_up' render={registrationProps => (
+                                <Registration {...registrationProps}
+                                              handleSuccessfulAuth={props.handleLogin}
+                                />
+                            )}/>
+                            <Route path="/posts" exact component={Home}/>
+                            <Route path="/posts/new" exact component={NewPost}/>
+                            <Route path="/posts/:id/edit" exact component={EditPost}/>
+                        </Switch>
+                    </div>
+                    <div className="cell large-4">
+                    </div>
+                </div>
             </div>
         </section>
     )

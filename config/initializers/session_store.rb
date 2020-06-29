43bc33d5c2044780ea3dbe4e89c10a44ed8b6082
Olga Pilipenco <pilip.olkin@gmail.com>
2020-06-29 17:48:00 -0600

    fix session store domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
                                          key: '_a_blog_app',
-                                         domain: 'vb-blog.herokuapp.com'
+                                         domain: 'yyc-match-point.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store,
                                          key: '_a_blog_app'

c476e45c0269e0bbea2f2d57615fa5a971ece59b
Olga Pilipenco <pilip.olkin@gmail.com>
2020-06-29 17:05:58 -0600

    force session load

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,7 +2,7 @@ module CurrentUserConcern
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_current_user
+    before_action :force_load_session, :set_current_user
     attr_reader :current_user
   end
 
@@ -10,5 +10,12 @@ module CurrentUserConcern
     return unless session[:user_id]
 
     @current_user = User.find(session[:user_id])
+  end
+
+  private
+
+  def force_load_session
+    # session is lazy loaded and for some reason doesn't load on read
+    session[:init] = 'BS' unless session.loaded?
   end
 end

eee9309dae4f0a691b5bdd632e122b393c1a2c48
Olga Pilipenco <pilip.olkin@gmail.com>
2020-06-29 19:12:26 -0600

    try css loader

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -14,6 +14,17 @@ const sassLoader = {
     ],
 }
 
+const cssLoader = {
+    test: /\.css$/i,
+    use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader'
+    ],
+}
+
 environment.loaders.append('sass', sassLoader)
+environment.loaders.append('css', cssLoader)
 
 module.exports = environment.toWebpackConfig()

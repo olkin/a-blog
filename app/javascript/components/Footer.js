5b51f084a133677ab704335a29ab725f4eb0bb67
Olga Pilipenco <pilip.olkin@gmail.com>
2020-07-06 15:24:09 -0600

    simplify footer

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -6,26 +6,10 @@ function Footer() {
     return (
         <div className="footer">
             <div className="wrap">
-                <div className="grid-x grid-padding-x small-up-1 medium-up-3">
-                    <div className="cell">
-                        <h4>Contact Info</h4>
-                        <hr/>
-                        <a href='#'><span>Phone</span> 555 555 55555 </a>
-                        <a href='#'><span>Email</span> email_here@whatever.com </a>
-                    </div>
-                    <div className="cell">
-                        <h4>Site Map</h4>
-                        <hr/>
-                        <a href='#'>About Us</a>
-                        <a href='#'>Contact Us</a>
-                    </div>
-                    <div className="cell">
-                        <h4>Social Media</h4>
-                        <hr/>
-                        <a href='#'>Facebook</a>
-                        <a href='#'>Twitter</a>
-                    </div>
-                </div>
+                <ul>
+                    <li><a href='#'>About Us</a></li>
+                    <li><a href='#'>Contact Us</a></li>
+                </ul>
             </div>
         </div>
     )

67b72524b5d32773f0350df9f5e1217874f627ee
Olga Pilipenco <pilip.olkin@gmail.com>
2020-07-07 09:31:56 -0600

    footer styles

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect, createContext} from 'react';
-import {Link, Route, Switch, useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import axios from 'axios';
 import Header from "./Header";
 import userContext from "./userContext";
 import Footer from "./Footer";
 import Main from "./Main";
-import Hero from "./Hero";
 import MobileMenu from "./MobileMenu";
 import '../styles/App.scss'
 
@@ -20,7 +19,7 @@ function App() {
         ).then(response => {
             if (response.data.logged_in && !isLoggedIn() ) {
                 setUser(response.data.user);
-            } else if (!response.data.logged_in && isLoggedIn()) {
+            } else if (!response.data.logged_in) {
                 setUser({});
             }
         }).catch(error => {
@@ -43,20 +42,13 @@ function App() {
     const isLoggedIn = () => user.email?.length > 0;
 
     return (
-        <div className="off-canvas-wrapper">
+        <div className="off-canvas-wrapper page-container">
             <div className="off-canvas position-left" id="mobile-menu" data-off-canvas>
                 <MobileMenu />
             </div>
             <div className="off-canvas-content" data-off-canvas-content>
                 <userContext.Provider value={{user: user}}>
                     <Header handleLogout={handleLogout}/>
-                    {/*{ isLoggedIn()*/}
-                    {/*    ? <></>*/}
-                    {/*    :*/}
-                    {/*    <Switch>*/}
-                    {/*        <Route exact path='/' component={Hero}/>*/}
-                    {/*    </Switch>*/}
-                    {/*}*/}
                     <Main handleLogin={handleLogin}/>
                     <Footer />
                 </userContext.Provider>

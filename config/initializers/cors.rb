807aa9f6a50eccf7c82c2e2f7b46d38b979743a5
Olga Pilipenco <pilip.olkin@gmail.com>
2020-07-08 12:17:21 -0600

    adjust cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://www.netsandballs.ca'
+    origins 'netsandballs.ca'
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],

7e64a31a7f574cba47d706727664260023638e53
Olga Pilipenco <pilip.olkin@gmail.com>
2020-07-08 12:08:13 -0600

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://vb-blog.herokuapp.com'
+    origins 'https://www.netsandballs.ca'
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],

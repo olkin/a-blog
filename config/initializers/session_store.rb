8784fddae5b7c64aac276eca53061d67716b6d43
Olga Pilipenco <pilip.olkin@gmail.com>
2020-07-08 11:45:47 -0600

    try different key for cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
-                                         key: '_a_blog_app',
+                                         key: '_nets_and_balls_app',
                                          domain: 'ballsandnets.ca'
 else
   Rails.application.config.session_store :cookie_store,

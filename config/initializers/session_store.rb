8cb4b1e003b2718fedc0774c71b4afabce550ed4
Olga Pilipenco <pilip.olkin@gmail.com>
2020-07-08 12:31:50 -0600

    session store fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
                                          key: '_nets_and_balls_app',
-                                         domain: 'ballsandnets.ca'
+                                         domain: 'netsandballs.ca'
 else
   Rails.application.config.session_store :cookie_store,
                                          key: '_a_blog_app'
